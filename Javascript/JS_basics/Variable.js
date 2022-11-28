cbf3543b20985346d8a4b3229a36201d8ea40c28
AzureDuulga <azure.duulga.enkhtur@gmail.com>
2022-11-28 17:36:47 +0800

    JS angid uzev

---
+++ Javascript/JS_basics/Variable.js
@@ -0,0 +1,66 @@
+// Exercise 1
+// "Comments can make code readable" 
+
+// Exercise 2
+// "Javascript lesson begin" 
+
+// Exercise 3
+/*3. "Comments can make cod
+e readable
+" олон мөртэй коммент болгох dfgdfg*/
+
+
+// Exercise 4
+let number = 28;
+let completed = true;
+let running = false;
+let home ;
+
+// Exercise 5
+console.log(typeof number);
+console.log(typeof completed);
+console.log(typeof running);
+console.log(typeof home);
+
+// Exercise 6
+let ner ;
+console.log(ner);
+
+// Exercise 7
+
+let hayg = "Baynzurkh" ;
+console.log(hayg);
+
+// Exercise 8
+let first_name = "Duulga"; let last_name = "Enkhtur"; let marital_status = "Single"; let country = "Mongol"; let age = 24;
+
+// Exercise 9
+let firstname = "Duulga"; 
+let lastname = "Enkhtur"; 
+let maritalstatus = "Single"; 
+let nation = "Mongol"; 
+let nas = 24;
+
+// Exercise 10
+let my_age = 24; 
+let your_age = 20; 
+
+// Exercise 11
+let x = 10;
+let y = 5;
+let z = 9;
+
+console.log("Гурвалжингийн периметр", x*y*z)
+
+// Exercise 12
+const r = 15;
+
+console.log("Тойргийн талбай", r**2)
+
+// Exercise 13
+console.log("m=",y**2-y**1/x**2-x**1)
+
+// Exercise 14
+
+const giga = 15;
+console.log("Byte=", giga*10737418240)
