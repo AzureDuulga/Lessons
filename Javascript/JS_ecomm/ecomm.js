45a451e0fd7ee5ca10fb8d6cc272336b0052ef44
AzureDuulga <azure.duulga.enkhtur@gmail.com>
2022-12-27 17:54:17 +0800

    ecomm my cart baraa nemev

--- Javascript/JS_ecomm/ecomm.js
+++ Javascript/JS_ecomm/ecomm.js
@@ -2,10 +2,14 @@ console.log("Ecommerce");
 // All variables
 const productList = document.getElementsByClassName("productList")[0];
 let allProducts = null;
+let cartList = document.getElementById("cartList");
+let cartProducts = [];
+let myCartDiv = document.getElementById("myCartDiv");
+let aside = document.getElementById("aside");
 
 const displayProduct = () => {
-  allProducts.forEach((product) => {
-    const item = `<div class="col-3 Cards p-2 rounded ">
+  allProducts.forEach((product, idx) => {
+    const productItem = `<div class="col-3 Cards p-2 rounded ">
                         <img
                         src="${product.thumbnail}"
                         alt=""
@@ -26,16 +30,16 @@ const displayProduct = () => {
                     </div>
                         <div class="col text-end">
                         <button
+                            onclick="addCart(${idx})"
                             class="text-primary border border-primary bg-white"
-                            id="watchBtn"
-                        >
-                            <i class="bi bi-heart"></i>
-                            Watch
+                            id="watchBtn">
+                            <i class="bi bi-bag"></i>
+                            Add to cart
                         </button>
                         </div>
                     </div>
                     </div>`;
-    productList.innerHTML += item;
+    productList.innerHTML += productItem;
   });
 };
 
@@ -48,3 +52,52 @@ const getProducts = async () => {
 };
 
 getProducts();
+
+const addCart = (idx) => {
+  console.log("Added to cart:", idx);
+  cartProducts.push(allProducts[idx]);
+  addToCart();
+};
+
+const addToCart = () => {
+  console.log(cartProducts);
+  cartList.innerHTML = "";
+  for (product of cartProducts) {
+    const cartItem = `        <div class="row">
+  <div class="col">
+    <div class="row">
+      <img
+        id="cartZurag"
+        src="${product.thumbnail}"
+        alt="Poduct"
+        class="col"
+      />
+      <p class="col">${product.title}</p>
+    </div>
+  </div>
+  <div class="col">
+    <div class="row text-center">
+      <div class="col">$${product.price}</div>
+      <div class="col">
+        <div class="row">
+          <div class="col">1</div>
+          <div class="col">
+            <div class="border">+</div>
+            <div class="border">-</div>
+          </div>
+        </div>
+      </div>
+      <div class="col">${product.price}</div>
+    </div>
+  </div>
+  <hr />
+</div>`;
+    cartList.innerHTML += cartItem;
+  }
+};
+
+const showCart = () => {
+  aside.classList.toggle("showCart");
+};
+
+//Niit sagsand bga buteegdehuunii niilber olhiin tuld cartProduct.reduce((total,value)=> {returen total+value},0)
